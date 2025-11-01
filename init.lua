5b05a3b41c23fa40924d5ca840276b0428222e41
Matt Garcia <strongpawcoding@gmail.com>
2025-11-01 16:05:29 -0500

    Up unitl here I have been using the setup from @Teej (idk his handle, that may be wrong). I ditched that for this very minimal config. This should be the first neovim with native lsp support.

--- init.lua
+++ init.lua
@@ -1,5 +1,24 @@
-require("config.lazy")
-
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.swapfile = false
+vim.opt.signcolumn = "yes"
+vim.g.mapleader = " "
+vim.opt.winborder = "rounded"
+
+
+vim.pack.add({
+	{ src = "https://github.com/folke/tokyonight.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+})
+
+vim.keymap.set('n', '<leader>oil', "<cmd>Oil<cr>", { desc = "Open parent directory" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+
+
+vim.lsp.enable({ "lua_ls" })
+
+vim.cmd("colorscheme tokyonight-moon")
